1d54ff53f3d931be5a20a41803f2a049d217386c
Mariana Luciano <mariana.luciano@aluno.ifsp.edu.br>
2023-05-02 11:28:17 -0300

    configuração inicial bdd

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+
+def sobre(request):
+    return HttpResponse("Esta é a página sobre do meu site")
