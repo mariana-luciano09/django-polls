07cf68c65af8df8db9e2ce088f9d5266515c6cc1
Mariana Luciano <mariana.luciano@aluno.ifsp.edu.br>
2023-05-10 09:10:05 -0300

    update models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=100, unique=True)
+    avatar = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.nickname
+
+
+class Questionario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500, null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True)
+    capa = models.CharField(max_length=225, null=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Hashtag(models.Model):
+    id = models.IntegerField(primary_key=True)
+    tag = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+
+class HashtagHasQuestionario(models.Model):
+    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+    questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('hashtag', 'questionario')
+
+    def __str__(self):
+        return f'{self.hashtag} - {self.questionario}'
