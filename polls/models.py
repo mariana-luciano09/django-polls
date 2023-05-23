24bd65828525701b9f0a6c8ad711c7e6e76b2619
Mariana Luciano <mariana.luciano@aluno.ifsp.edu.br>
2023-05-23 11:25:18 -0300

    alteração no models

--- polls/models.py
+++ polls/models.py
@@ -5,10 +5,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
-
-#Create yout model here
+    
+    def __str__(self):
+        return self.question_text
