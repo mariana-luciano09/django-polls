66553e7aab7839e33b0667b14e0a3a460c9d9313
Mariana Luciano <mariana.luciano@aluno.ifsp.edu.br>
2023-09-05 11:14:20 -0300

    alteracoes 0509

--- polls/urls.py
+++ polls/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
 
     path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),
 
-    path('<int:pk>', views.QuestionDetailView.as_view(), name='question-detail')
-]
+    path('<int:pk>', views.QuestionDetailView.as_view(), name='question-detail'),
 
+    path('<int:pk>/deletar', views.QuestionDeleteView.as_view(), name='question-delete')
+]
