700414c0f35cd7da97dcd9425553fffc95c98ad2
Mariana <mariana.luciano@aluno.ifsp.edu.br>
2023-10-10 11:20:40 -0300

    alteracoes 1010

--- polls/views.py
+++ polls/views.py
@@ -53,6 +53,49 @@ class QuestionDeleteView(LoginRequiredMixin, DeleteView):
 
 class QuestionUpdateView(UpdateView):
     model = Question
-    success_url:reverse_lazy('question_list')
-    fields = ('question-text', )
-          
+    templates_name = 'polls/question_form.html'
+    success_url = reverse_lazy('question_list')
+    success_message = 'Pergunta atualizada'
+    fields = ('question-text', 'pub_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando a pergunta'
+
+
+        question_id = self.kwargs.get('pk')
+        choices = Choice.objects.filter(question_pk=question_id)
+        context['question_choices'] = choices
+
+        return context
+    
+    def form_valid(self, request, *args, **kwargs):
+        messages.sucess(self.request, self.success_message)
+        return super(QuestionUpdateView, self).form_valid(request, *args, **kwargs)
+
+class choicecreateView(createView):
+    model = Choice
+    template_name = 'polls/choice_form.html'
+    fields = ('choice_text', )
+    success_message = 'Alternativa registrada com sucesso!'
+
+ def dispatch(self, request, *args, **kwargs):
+    self.question = get_object_or_404(Question, pk=self.kwargs.get('pk'))
+    return super (Choicecreateview, self).dispatch( request, *args, **kwargs)
+
+def get_context_data(self, **kwargs):
+    question = get_object_or_404(Question, pk=self.kwargs.get('pk'))
+
+    context = super(Choicecreateview, self).get_context_data(**kwargs)
+    context ['form_title'] = 'Alternativa para: {question.question_text}'
+    
+    return context
+
+def form_valid(self, form):
+    form.instance.question = self.question
+    messages.success(self.request, self.success_message)
+    return super (Choicecreateview, self).form_valid(form)
+
+def get_success_ur1(self, *args, **kwargs):
+    question_id = self.kwargs.get('pk')
+    return reverse lazy('poll_edit', kwargs-('pk': question_id7)
