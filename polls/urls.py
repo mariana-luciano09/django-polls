d90a00acc1d6d82ac2a07876d3423e957896a68b
Mariana Luciano <mariana.luciano@aluno.ifsp.edu.br>
2023-11-07 10:56:28 -0300

    alteracoes 0711

--- polls/urls.py
+++ polls/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     
     path('alternativa/<int:pk>/edit', views.ChoiceUpdateView.as_view(), name="choice_edit"),
     
-    path('alternativa/<int:pk>/delete', views.ChoiceDeleteView.as_view(), name="choice_delete")
+    path('alternativa/<int:pk>/delete', views.ChoiceDeleteView.as_view(), name="choice_delete"),
+
+    path('pergunta/<int:question_id>/vote', views.vote, name="poll_vote"),
+    
 ]
